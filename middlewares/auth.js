138bb67764d3cf925c3a74e69fad3e1f5f7d5a30
Kirill Sinitsyn <k.s.sinitsyn@gmail.com>
2019-11-10 19:03:43 +0300

    Все готово к деплою, но проблема с доступом ssh к яндекс облаку не решена, пока заливаю без ссылок на домены и IP

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const Error401 = require('../errors/401');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
@@ -6,12 +7,11 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, process.env.SECRET_KEY);
-  } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    payload = jwt.verify(token, process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'some-secret-key');
+  } catch (error) {
+    next(new Error401('Необходима авторизация'));
   }
+
   req.user = payload;
 
   next();
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,13 @@
 const users = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const { createUser, getAllUsers, getSingleUser } = require('../controllers/users');
 
 users.get('/', getAllUsers);
-users.get('/:id', getSingleUser);
+users.get('/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().alphanum(),
+  }),
+}), getSingleUser);
 users.post('/', createUser);
 
 module.exports = users;
